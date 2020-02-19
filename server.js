d81cf6f9854a181c36b88f361483dcef47a842a8
Carlonn Rivers <danidre14@gmail.com>
2020-02-19 17:20:06 -0400

    Defined path in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+const path = require("path");
 
 const publicRouter = require("./routes/index");
 const apiRouter = require("./api/index");
