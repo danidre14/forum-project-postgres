98df6d9bc10bea09cbf4219232525b13ace9e911
Carlonn Rivers <danidre14@gmail.com>
2020-02-19 20:34:08 -0400

    Limit where id < 20000

--- db/queries.js
+++ db/queries.js
@@ -5,7 +5,9 @@ module.exports.Posts = {
         return knex("posts").insert(post, "*");
     },
     read(id) {
-        return id ? knex("posts").where("id", id).first() : knex("posts").orderBy("id", "desc").limit(8);
+        return id ? knex("posts").where("id", id).first() : knex("posts").where("id", "<", 20000).orderBy("id", "desc");
+
+        //knex("posts").limit(8);
     },
     update(id, post) {
         return knex("posts").where("id", id).update(post, "*");
