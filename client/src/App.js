0822a35cbf8f1ee716d65b611baf1cad170dcfe9
Carlonn Rivers <danidre14@gmail.com>
2020-02-20 02:40:12 -0400

    Added Heroku postbuild proper spelling saved

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ function App() {
                 <Route path="/posts/view" component={PostsView} />
                 <Route path="/" component={RedirectHomePage} />
             </Switch>
+
         </Router>
     );
 }
